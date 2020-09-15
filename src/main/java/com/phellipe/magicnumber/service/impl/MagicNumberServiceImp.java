8b3239d12ddaef22ce7c7867a2989c0afcbeabb1
Phellipe Rodrigues <phelliperodrigues.dev@gmail.com>
2020-09-15 20:39:18 -0300

    Implementation countMagicNumber() in MagicNumberServiceImp

--- src/main/java/com/phellipe/magicnumber/service/impl/MagicNumberServiceImp.java
+++ src/main/java/com/phellipe/magicnumber/service/impl/MagicNumberServiceImp.java
@@ -4,12 +4,34 @@ import com.phellipe.magicnumber.model.Number;
 import com.phellipe.magicnumber.service.MagicNumberService;
 import org.springframework.stereotype.Service;
 
-import java.util.List;
+import java.util.*;
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.stream.IntStream;
 
 @Service
 public class MagicNumberServiceImp implements MagicNumberService {
+
     @Override
     public int countMagicNumber(List<Number> numbers) {
-        return 0;
+        AtomicInteger count = new AtomicInteger();
+
+        numbers.forEach(number -> {
+            for (int i = number.getNumberA(); i <= number.getNumberB() ; i++) {
+                if (isSquareRoot(i) && isPrime((int) Math.sqrt(i)))
+                    count.getAndIncrement();
+            }
+        });
+
+        return count.get();
     }
+
+    private boolean isPrime(int number) {
+        return IntStream.rangeClosed(2, number/2).noneMatch(i -> number%i == 0);
+    }
+
+    private boolean isSquareRoot(int number){
+        double squareRoot = Math.sqrt(number);
+        return ((squareRoot - Math.floor(squareRoot)) == 0);
+    }
+
 }
