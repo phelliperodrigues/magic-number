e3a5f4874a74019269392c69752b82564bf28c58
Phellipe Rodrigues <phelliperodrigues.dev@gmail.com>
2020-09-15 21:01:44 -0300

    Implementation Rest Controller from count Magic Number

--- src/main/java/com/phellipe/magicnumber/controller/MagicNumberController.java
+++ src/main/java/com/phellipe/magicnumber/controller/MagicNumberController.java
@@ -1,4 +1,31 @@
 package com.phellipe.magicnumber.controller;
 
+import com.phellipe.magicnumber.controller.dto.MagicNumber;
+import com.phellipe.magicnumber.model.Number;
+import com.phellipe.magicnumber.service.MagicNumberService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/magicnumber")
 public class MagicNumberController {
+
+    final
+    MagicNumberService service;
+
+    public MagicNumberController(MagicNumberService service) {
+        this.service = service;
+    }
+
+    @PostMapping
+    public MagicNumber count(@RequestBody List<Number> numbers) {
+        int count = service.countMagicNumber(numbers);
+
+        return MagicNumber.builder().count(count).build();
+    }
 }
